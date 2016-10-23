67d7bc4b1280b7d7662d595e6a3b428a89cc1808
david.chang <chechiachang999@gmail.com>
2016-10-24 01:27:39 +0800

    Finishing exercise 1:

--- src/main/scala/week1/recfun/src/main/scala/recfun/Main.scala
+++ src/main/scala/week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,12 +13,28 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if (r < 2) 1
+      else if (c == 0) 1
+      else pascal(c - 1, r - 1) + pascal(c, r -1)
+    }
+
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      if (chars.isEmpty) true
+      else if (chars.head == "(" && findClose(chars.tail))
+        balance(chars.tail)
+      else false
+
+      def findClose(chars: List[Char]): Boolean = {
+        if (chars.isEmpty) false
+        else if (chars.head == ")") true
+        else findClose(chars.tail)
+      }
+    }
   
   /**
    * Exercise 3
