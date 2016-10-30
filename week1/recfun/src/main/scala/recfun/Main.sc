748445e46f7f4a0081111f9ce219ae6d59fa0174
david.chang <chechiachang999@gmail.com>
2016-10-30 10:50:51 +0800

    Week1 Finishing practice to get 10 points

--- week1/recfun/src/main/scala/recfun/Main.sc
+++ week1/recfun/src/main/scala/recfun/Main.sc
@@ -1,4 +1,19 @@
 object recfun {
+
+  println("Pascal's Triangle")
+  for (row <- 0 to 10) {
+    for (col <- 0 to row)
+      print(pascal(col, row) + " ")
+    println()
+  }
+
+  def pascal(c: Int, r: Int): Int = {
+    if (r < 2) 1
+    else if (c == 0) 1
+    else if (c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r -1)
+  }
+
   def balance(chars: List[Char]): Boolean = {
 
     def pairOpen(count: Int, chars: List[Char]): Boolean = {
@@ -25,22 +40,25 @@ object recfun {
 
   //Count change
   def countChange(money: Int, coins: List[Int]): Int = {
+
+
     def degnerate(money: Int, coins: List[Int], count: Int): Int = {
       if (coins.isEmpty)
         count
       // use all coins in hand, stop and return current solutions count
       else if (money == 0)
-        degnerate(money, coins.tail, count + 1)
+        1
       // find way to pay, add 1 to solution count and try next coins
       else if (money - coins.head < 0)
-        degnerate(money, coins.tail, count)
+        count + degnerate(money, coins.tail, 0)
       // coins.head will exceed money, use next coin instead
       else
-        degnerate(money - coins.head, coins, degnerate(money - coins.head, coins.tail, 0))
+        degnerate(money - coins.head, coins, 0) +
+          degnerate(money, coins.tail, 0)
       // coins.head won't exceed, use head coin to pay
     }
 
-    if (money == 0) 1 else degnerate(money, coins, 0)
+    degnerate(money, coins, 0)
   }
 
   countChange(0, List(1, 2))
--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,11 +13,12 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = {
-      if (r < 2) 1
-      else if (c == 0) 1
-      else pascal(c - 1, r - 1) + pascal(c, r -1)
-    }
+  def pascal(c: Int, r: Int): Int = {
+    if (r < 2) 1
+    else if (c == 0) 1
+    else if (c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r -1)
+  }
 
   
   /**
@@ -28,7 +29,7 @@ object Main {
     def pairOpen(count: Int, chars: List[Char]): Boolean = {
 
       if (chars.isEmpty)
-        if (count == 0) true else false
+        count == 0
       else if (count < 0) false
       else if (chars.head == '(') pairOpen(count + 1, chars.tail)
       else if (chars.head == ')') pairOpen(count - 1, chars.tail)
@@ -47,16 +48,17 @@ object Main {
         count
       // use all coins in hand, stop and return current solutions count
       else if (money == 0)
-        degnerate(money, coins.tail, count + 1)
+        1
       // find way to pay, add 1 to solution count and try next coins
       else if (money - coins.head < 0)
-        degnerate(money, coins.tail, count)
+        count + degnerate(money, coins.tail, 0)
       // coins.head will exceed money, use next coin instead
       else
-        degnerate(money - coins.head, coins, degnerate(money - coins.head, coins.tail, 0))
+        degnerate(money - coins.head, coins, 0) +
+          degnerate(money, coins.tail, 0)
       // coins.head won't exceed, use head coin to pay
     }
 
-    if (money == 0) 1 else degnerate(money, coins, 0)
+    degnerate(money, coins, 0)
   }
-  }
+}
