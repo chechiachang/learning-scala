c200d63b144de68bfb86eb1173667e6849955f45
chechiachang <chechiachang999@gmail.com>
2017-01-17 08:40:54 +0800

    WIP

---
+++ programming-in-scala/src/main/scala/playground/Combat.scala
@@ -0,0 +1,8 @@
+package playground
+
+/**
+  * Created by David Chang on 2017/1/17.
+  */
+class Combat (p1: HumanWarrior, p2: HumanWarrior){
+
+}
--- programming-in-scala/src/main/scala/playground/Object.scala
+++ programming-in-scala/src/main/scala/playground/Object.scala
@@ -1,4 +1,4 @@
-package ch6
+package playground
 
 /**
   * Created by David Chang on 2017/1/17.
